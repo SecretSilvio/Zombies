d0d6c11afb8adc4366ccc48c75d9259faf4f6622
SecretSilvio <nmc322@drexel.edu>
2025-05-05 17:10:21 -0400

    more nodes for behavior tree

--- ZombiesGame/Assets/Scripts/AI/Node.cs
+++ ZombiesGame/Assets/Scripts/AI/Node.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEditor;
 
 namespace Pathfinding.BehaviorTrees
 {
@@ -23,11 +24,96 @@ namespace Pathfinding.BehaviorTrees
         }
     }
 
+    public class PrioritySelector : Selector
+    {
+        List<Node> sortedChildren;
+        List<Node> SortedChildren => sortedChildren ??= SortChildren();
+
+        protected virtual List<Node> SortChildren()
+        {
+            return children.OrderByDescending(child => child.priority).ToList();
+        }
+
+        public PrioritySelector(string name) : base(name) { }
+
+        public override void Reset()
+        {
+            base.Reset();
+            sortedChildren = null;
+        }
+
+        public override Status Process()
+        {
+            foreach (var child in SortedChildren)
+            {
+                switch (child.Process())
+                {
+                    case Status.Running:
+                        return Status.Running;
+                    case Status.Success:
+                        return Status.Success;
+                    default:
+                        continue;
+                }
+            }
+
+            return Status.Failure;
+        }
+    }
+
+    public class Selector : Node
+    {
+        public Selector(string name, int priority = 0) : base(name, priority) { }
+        public override Status Process()
+        {
+            if (currentChild < children.Count)
+            {
+                switch (children[currentChild].Process())
+                {
+                    case Status.Running:
+                        return Status.Running;
+                    case Status.Success:
+                        Reset();
+                        return Status.Success;
+                    default:
+                        currentChild++;
+                        return Status.Running;
+                }
+            }
+            Reset();
+            return Status.Failure;
+        }
+    }
+
+    public class Sequence : Node
+    {
+        public Sequence(string name, int priority = 0) : base(name, priority) { }
+        public override Status Process()
+        {
+            if (currentChild < children.Count)
+            {
+                switch (children[currentChild].Process()) {
+                    case Status.Running:
+                        return Status.Running;
+                    case Status.Failure:
+                        Reset();
+                        return Status.Failure;
+                    default:
+                        currentChild++;
+                        return currentChild == children.Count ? Status.Success : Status.Running;
+                }
+            }
+
+            Reset();
+            return Status.Success;
+        }
+    }
+
     public class Leaf : Node
     {
         readonly IStrategy strategy;
 
-        public Leaf(string name, IStrategy strategy) : base(name)
+        public Leaf(string name, IStrategy strategy, int priority = 0) : base(name, priority)
         {
             this.strategy = strategy;
         }
@@ -47,12 +133,15 @@ namespace Pathfinding.BehaviorTrees
         }
 
         public readonly string name;
+        public readonly int priority;
+
         public readonly List<Node> children = new();
         protected int currentChild;
 
-        public Node(string name = "Node")
+        public Node(string name = "Node", int priority = 0)
         {
             this.name = name;
+            this.priority = priority;
         }
 
         public void AddChild(Node child) => children.Add(child);
--- ZombiesGame/Assets/Scripts/AI/Strategies.cs
+++ ZombiesGame/Assets/Scripts/AI/Strategies.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,7 +8,37 @@ namespace Pathfinding.BehaviorTrees
     public interface IStrategy
     {
         Node.Status Process();
-        void Reset();
+        void Reset()
+        {
+            // Default implementation does nothing
+        }
+    }
+
+    public class  ActionStrategy : IStrategy
+    {
+        readonly Action doSomething;
+
+        public ActionStrategy(Action doSomething)
+        {
+            this.doSomething = doSomething;
+        }
+
+        public Node.Status Process()
+        {
+            doSomething();
+            return Node.Status.Success;
+        }
+    }
+
+    public class Condition : IStrategy
+    {
+        readonly Func<bool> predicate;
+
+        public Condition(Func<bool> predicate)
+        {
+            this.predicate = predicate;
+        }
+        public Node.Status Process() => predicate() ? Node.Status.Success : Node.Status.Failure;
     }
 
     public class PatrolStrategy : IStrategy
