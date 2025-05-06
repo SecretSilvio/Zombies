5c62db88299ac27cad64584b9715081af3d4e458
Hgallo42 <hgallo42@gmail.com>
2025-05-05 23:22:25 -0400

    Removed Audio

--- ZombiesGame/Assets/Scripts/UI/menu.cs
+++ ZombiesGame/Assets/Scripts/UI/menu.cs
@@ -3,11 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public AudioSource audioSource;
-    public AudioClip onClickClip;
 
     public void ClickAndLoad() {
-        audioSource.PlayOneShot(onClickClip);
         Invoke("LoadGame", 0.5f);
     }
     
