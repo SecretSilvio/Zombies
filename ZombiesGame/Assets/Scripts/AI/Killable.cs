5579cecd22b39a0a26b3ec2ccc1ab67ed1784691
SecretSilvio <nmc322@drexel.edu>
2025-05-12 22:16:13 -0400

    npc attack and death anims

--- ZombiesGame/Assets/Scripts/AI/Killable.cs
+++ ZombiesGame/Assets/Scripts/AI/Killable.cs
@@ -1,9 +1,27 @@
+using System.Collections;
 using UnityEngine;
 
 public class Killable : MonoBehaviour
 {
+    private Animator animator;
+
+    public void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     public void Die()
     {
-        Destroy(gameObject); // Replace with death animation or effects
+        StartCoroutine(DieCoroutine());
+    }
+
+    IEnumerator DieCoroutine()
+    {
+        // Play death animation
+        animator.SetTrigger("Death");
+        // Disable any components that should not be active after death
+        // Wait for a few seconds before destroying the object
+        yield return new WaitForSeconds(4f);
+        Destroy(gameObject);
     }
 }
